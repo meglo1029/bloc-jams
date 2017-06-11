abf407551c116d05ffa6826e7d4165e59a8a42eb
Meghan Logan <meglo1029@gmail.com>
2017-06-11 12:44:21 -0700

    all changes to checkpoint-19

--- scripts/album.js
+++ scripts/album.js
@@ -1,72 +1,3 @@
-var albumPicasso = {
-	title: 'The Colors',
-	artist: 'Pablo Picasso',
-	label: 'Cubism',
-	year: '1881',
-	albumArtUrl: 'assets/images/album_covers/01.png',
-	songs: [{
-		title: 'Blue',
-		duration: '4:26'
-	}, {
-		title: 'Green',
-		duration: '3:14'
-	}, {
-		title: 'Red',
-		duration: '5:01'
-	}, {
-		title: 'Pink',
-		duration: '3:21'
-	}, {
-		title: 'Magenta',
-		duration: '2:15'
-	}, ]
-};
-var albumMarconi = {
-	title: 'The Telephone',
-	artist: 'Guglielmo Marconi',
-	label: 'EM',
-	year: '1909',
-	albumArtUrl: 'assets/images/album_covers/20.png',
-	songs: [{
-		title: 'Hello, Operator?',
-		duration: '1:01'
-	}, {
-		title: 'Ring, ring, ring',
-		duration: '5:01'
-	}, {
-		title: 'Fits in your pocket',
-		duration: '3:21'
-	}, {
-		title: 'Can you hear me now?',
-		duration: '3:14'
-	}, {
-		title: 'Wrong phone number',
-		duration: '2:15'
-	}]
-};
-var albumFuturama = {
-	title: 'The Year Three Thousand',
-	artist: 'Phillip J Fry',
-	label: 'FOX',
-	year: '1999',
-	albumArtUrl: 'assets/images/album_covers/16.png',
-	songs: [{
-		title: 'Bender',
-		duration: '4:26'
-	}, {
-		title: 'Leela',
-		duration: '3:14'
-	}, {
-		title: 'Amy',
-		duration: '5:01'
-	}, {
-		title: 'Zoidberg',
-		duration: '3:21'
-	}, {
-		title: 'Professor',
-		duration: '2:15'
-	}]
-};
 var createSongRow = function(songNumber, songName, songLength) {
 	var template = '<tr class="album-view-song-item">' + '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>' + '  <td class="song-item-title">' + songName + '</td>' + '  <td class="song-item-duration">' + songLength + '</td>' + '</tr>';
     var $row = $(template);  
@@ -110,13 +41,16 @@ var createSongRow = function(songNumber, songName, songLength) {
     return $row;
 };
 
-var $albumTitle = $('.album-view-title');
-var $albumArtist = $('.album-view-artist');
-var $albumReleaseInfo = $('.album-view-release-info');
-var $albumImage = $('.album-cover-art');
-var $albumSongList = $('.album-view-song-list');
 var subscribed = false;
 var setCurrentAlbum = function(album) {
+    currentAlbum = album;
+    
+    var $albumTitle = $('.album-view-title');
+    var $albumArtist = $('.album-view-artist');
+    var $albumReleaseInfo = $('.album-view-release-info');
+    var $albumImage = $('.album-cover-art');
+    var $albumSongList = $('.album-view-song-list');
+        
      $albumTitle.text(album.title);
      $albumArtist.text(album.artist);
      $albumReleaseInfo.text(album.year + ' ' + album.label);
@@ -137,7 +71,9 @@ var setCurrentAlbum = function(album) {
 
 var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
 var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
-var currentlyPlayingSong = null;
+var currentAlbum = null;
+var currentlyPlayingSongNumber = null;
+var currentSongFromAlbum = null;
 
 //var findParentByClassName = function(element, targetClass) {
 //	if (element) {
